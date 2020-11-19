6b04d03c2bfb7b12a0237b412f75871b008e1dfb
monali-source <56391792+monali-source@users.noreply.github.com>
2020-11-19 13:42:49 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(27, 12)
+        Me.AxWindowsMediaPlayer1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(830, 272)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(280, 469)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Démarrer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(921, 469)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 29)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Arreter"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 46)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(1203, 517)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(144, 338)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 29)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(144, 378)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 29)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Label4"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label4.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1372, 512)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,159 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    'Compteur text  a affiche   
+    Dim CompteurSec As Integer = 0
+    'numero de sous titres
+    Dim Numero(5) As String
+    'duree de sous titres
+    Dim Duree(5) As String
+    'texte line premier
+    Dim Texte1(5) As String
+    'texte line 2eme
+    Dim Texte2(5) As String
+    'convertion tableux donne a chaine de caracter
+    Dim TableauToChaine As String
+    ' calcul minutes en sec
+    Dim CalculMinutes1 As Integer
+    Dim CalculMinutes2 As Integer
+    Dim CalculSecondes1 As Integer
+    Dim CalculSecondes2 As Integer
+    Dim TotalSecondes As Integer
+    Dim CalculMinutesStop1 As Integer
+    Dim CalculMinutesStop2 As Integer
+    Dim CalculSecondesStop1 As Integer
+    Dim CalculSecondesStop2 As Integer
+    Dim TotalSecondesStop As Integer
+    Dim SaveTotalSecondes As Integer = 0 ' egale 0 a camancee par 0 
+    Dim SaveTotalSecondesStop As Integer = 0
+
+
+    'compteur caracter de la chaine
+    Dim k As Integer
+    ' compteur pousition d position dans la chaine de caracter
+    Dim d As Integer = 1
+
+
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Dim myFileToRea1d As New System.IO.StreamReader("‪‪‪C:\Users\patel\Downloads\Projet1\sous-titres.txt", False)
+        Dim myFileToRead1 As String
+        myFileToRead1 = "C:\Users\patel\Downloads\Projet1\sous-titres.txt"
+        Dim myFileToRead = My.Computer.FileSystem.OpenTextFileReader(myFileToRead1)
+
+
+        'Saisir la quantité de soustitres dans le fichier
+        For i = 1 To 5
+
+            'Lire la premire ligne du texte
+            Numero(i) = myFileToRead.ReadLine()
+            'Lire deuxième ligne
+            Duree(i) = myFileToRead.ReadLine()
+            'Lire troisieme ligne
+            Texte1(i) = myFileToRead.ReadLine()
+            'Lire quatrieme ligne 
+            Texte2(i) = myFileToRead.ReadLine()
+
+        Next i
+
+        'Fermer le fichier 
+        myFileToRead.Close()
+       
+        'Convertir Tableau Duree en Chaine de caracteres
+        TableauToChaine = String.Join(",", Duree.ToArray())
+
+
+
+
+        
+
+        
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' lance video click sur button demarre 
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+        'lance timer
+        Timer1.Enabled = True
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'arrete video
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+        'arrete timer
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'comptage de la chaine de caracter
+        If k < TableauToChaine.Length Then
+            'si on trouve un , dans la chaine alors on calcul sec 
+            If (TableauToChaine(k) = ",") Then
+
+                ''START DES SOUS-TITRES
+                '    'Calcul Minutes en secondes
+                CalculMinutes1 = (Val(TableauToChaine(d))) * 10 * 60
+                CalculMinutes2 = CalculMinutes1 + (Val(TableauToChaine(d + 1)) * 60)
+                '    'Calcul secondes
+                CalculSecondes1 = (Val(TableauToChaine(d + 3))) * 10
+                CalculSecondes2 = CalculSecondes1 + (Val(TableauToChaine(d + 4)))
+                '    'Total Secondes
+                TotalSecondes = CalculMinutes2 + CalculSecondes2
+
+                ''STOP DES SOUS-TITRES
+                '    'Calcul Minutes en secondes
+                CalculMinutesStop1 = (Val(TableauToChaine(d + 10))) * 10 * 60
+                CalculMinutesStop2 = CalculMinutesStop1 + (Val(TableauToChaine(d + 11)) * 60)
+                '    'Calcul secondes
+                CalculSecondesStop1 = (Val(TableauToChaine(d + 13))) * 10
+                CalculSecondesStop2 = CalculSecondesStop1 + (Val(TableauToChaine(d + 14)))
+                '    'Total Secondes
+                TotalSecondesStop = CalculMinutesStop2 + CalculSecondesStop2
+
+                Label1.Text = "Start Sout-titres  " & TotalSecondes & "   Stop Sous-Titres  " & TotalSecondesStop
+
+                CompteurSec += 1
+            End If
+
+            d = d + 1
+            k = k + 1
+
+            Label2.Text = "compteur  " & k
+
+
+            'Afficher les sous-titres
+
+            If k >= TotalSecondes Then
+                Label3.Visible = True
+                Label4.Visible = True
+                Label3.Text = Texte1(CompteurSec)
+
+                Label4.Text = Texte2(CompteurSec)
+
+            End If
+
+            If k >= TotalSecondesStop Then
+                Label3.Visible = False
+                Label4.Visible = False
+            End If
+        End If
+
+    End Sub
+
+    Private Sub AxWindowsMediaPlayer1_Enter(sender As Object, e As EventArgs) Handles AxWindowsMediaPlayer1.Enter
+
+    End Sub
+End Class
